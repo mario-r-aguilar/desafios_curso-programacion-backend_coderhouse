eac4756b020e09c2f8c2b35667682f7a4d1237a8
Mario Aguilar <mariocba150@gmail.com>
2023-04-23 17:55:18 -0300

    agrego métodos updateProduct - deleteProduct

--- desafio_02/productManagerV2.js
+++ desafio_02/productManagerV2.js
@@ -5,19 +5,18 @@ class productManager {
 		this.path = path;
 		fs.promises.writeFile(this.path, JSON.stringify([]));
 	}
+	//Variable privada del id de producto
 	#id = 0;
-	// Método privado para leer archivos
+	// Método privado para leer los productos almacenado en el archivo
 	#readFile = async () => {
 		const readProduct = await fs.promises.readFile(this.path, 'utf-8');
 		return JSON.parse(readProduct);
 	};
-
 	// Método privado para incrementar id automáticamente
 	#getId() {
 		this.#id++;
 		return this.#id;
 	}
-
 	// Método para mostrar lista de productos
 	getProducts = async () => {
 		let showProducts = await this.#readFile();
@@ -32,23 +31,20 @@ class productManager {
 	 * @param {string} code (código identificador único del producto)
 	 * @param {number} stock (número de piezas disponibles del producto)
 	 */
-
 	addProduct = async (title, description, price, thumbnail, code, stock) => {
 		// Valido que se hayan completado todos los campos
 		if (!title || !description || !price || !thumbnail || !code || !stock) {
 			console.log('Faltan datos. Por favor intente nuevamente');
 			return;
 		}
-
+		//Almaceno contenido del archivo de productos en una variable
 		let products = await this.#readFile();
-
 		// Valido que no haya códigos duplicados
 		const duplicateCode = products.find((product) => product.code === code);
 		if (duplicateCode) {
 			console.log('El código ya existe');
 			return;
 		}
-
 		// Creo el producto
 		const newProduct = {
 			title,
@@ -58,38 +54,82 @@ class productManager {
 			code,
 			stock,
 		};
-
 		// Genero id automáticamente y la agrego al producto
 		newProduct.id = this.#getId();
-
-		// Agrego el producto a la lista de productos
+		// Agrego el producto a la variable de productos
 		products.push(newProduct);
-
+		//Agrego los productos al archivo de almacenamiento
 		await fs.promises.writeFile(this.path, JSON.stringify(products));
 	};
 
+	/** Método para buscar un producto por id
+	 * @param {number} id (Id del producto)
+	 */
 	getProductById = async (id) => {
+		//Almaceno contenido del archivo de productos en una variable
 		let products = await this.#readFile();
-
+		//Busco el índice del producto solicitado por id
 		const findProductIndex = await products.findIndex(
 			(product) => product.id === id
 		);
-
-		// Valida que el id exista y muestra un mensaje si no lo encuentra
+		// Valido que el id exista y muestra un mensaje si no lo encuentra
 		if (findProductIndex === -1) {
 			console.log('Not found');
 			return;
 		}
-
-		// Muestra el producto solicitado
+		// Muestro el producto solicitado
 		const productFound = Object.entries(products[findProductIndex]);
 		console.log(`Producto Encontrado! 
         ${productFound}`);
 	};
 
-	// 	updateProduct(id, field) {}
+	/** Método para actualizar los campos y valores de un producto
+	 * @param {object} {objetoDesestructurado} (Producto a modificar)
+	 */
+	updateProduct = async ({ id, ...product }) => {
+		//Valido que se incluyan todos los campos del producto que se ingresa
+		if (
+			!id ||
+			!product.title ||
+			!product.description ||
+			!product.price ||
+			!product.thumbnail ||
+			!product.code ||
+			!product.stock
+		) {
+			console.log(`Faltan campos en el producto ingresado. Recuerde que debe contar con la siguiente información: 
+			title,
+			description,
+			price,
+			thumbnail,
+			code,
+			stock,
+			id,`);
+		} else {
+			//Elimino el producto a modificar
+			await this.deleteProduct(id);
+			//Guardo el listado de productos restantes en una variable
+			let beforeProducts = await this.#readFile();
+			//Genero un nuevo listado de productos con el modificado y los que no se vieron afectados
+			let afterProducts = [{ ...product, id }, ...beforeProducts];
+			//Agrego el nuevo listado al archivo de productos
+			await fs.promises.writeFile(this.path, JSON.stringify(afterProducts));
+			console.log('Producto actualizado');
+		}
+	};
 
-	// 	deleteProduct(id) {}
+	/** Método para eliminar un producto por id
+	 * @param {number} id (Id del producto)
+	 */
+	deleteProduct = async (id) => {
+		//Almaceno contenido del archivo de productos en una variable
+		let products = await this.#readFile();
+		//Creo un nuevo listado sin el producto eliminado
+		let productFilter = products.filter((products) => products.id != id);
+		//Agrego los productos al archivo de almacenamiento
+		await fs.promises.writeFile(this.path, JSON.stringify(productFilter));
+		console.log('Operación realizada con éxito');
+	};
 }
 
 // Pruebas
@@ -99,23 +139,41 @@ const test = async () => {
 	try {
 		await productList.getProducts();
 		await productList.addProduct(
-			'prueba',
-			'prueba',
-			200,
+			'celular a3',
+			'celular samsung ',
+			20000,
 			'sin imagen',
-			'CODE200',
+			'SAMSUNG-A3',
 			200
 		);
 		await productList.addProduct(
-			'prueba2',
-			'prueba2',
-			300,
+			'celular g6',
+			'celular motorola',
+			30000,
 			'sin imagen2',
-			'CODE300',
+			'MOTOROLA-G6',
 			300
+		);
+		await productList.addProduct(
+			'celular iphone',
+			'celular iphone 14',
+			50000,
+			'sin imagen2',
+			'IPHONE-14',
+			500
 		);
 		await productList.getProducts();
 		await productList.getProductById(2);
+		await productList.deleteProduct(3);
+		await productList.updateProduct({
+			title: 'celular a3',
+			description: 'celular samsung ',
+			price: 100000,
+			thumbnail: 'sin imagen',
+			code: 'SAMSUNG-A3-NEW_PRICE',
+			stock: 200,
+			id: 1,
+		});
 	} catch (error) {
 		console.log('Revisar código');
 	}
