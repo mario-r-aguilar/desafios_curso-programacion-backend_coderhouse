c6a89db93109357ca40eb3622afefc5a249dffa8
Mario Aguilar <mariocba150@gmail.com>
2023-04-28 21:56:38 -0300

    corrijo parentesis del segundo get

--- desafio_03/src/app.js
+++ desafio_03/src/app.js
@@ -6,17 +6,16 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 const productList = new productManager('./src/productos');
-const showProductList = await productList.getProducts();
+const showProductList = productList.getProducts();
 
 app.get('/products', async (req, res) => {
-	console.log(showProductList);
 	try {
 		let limit = parseInt(req.query.limit);
 		if (!limit) {
-			return res.send(console.log(showProductList));
+			return res.send(await showProductList);
 		} else {
-			//let allProduct = showProductList;
-			let productLimit = showProductList.slice(0, limit);
+			let allProduct = await showProductList;
+			let productLimit = allProduct.slice(0, limit);
 			res.send(await productLimit);
 		}
 	} catch (error) {
