28998337c1d919a0f6450cdf6eb3c82967f9174b
Mario Aguilar <mariocba150@gmail.com>
2023-05-08 14:13:33 -0300

    agrego comentarios de cartsManager.js

--- desafio_4-primera_preentrega/src/controllers/cartsManager.js
+++ desafio_4-primera_preentrega/src/controllers/cartsManager.js
@@ -1,7 +1,9 @@
+// Importo fs, nanoid (para generar id automáticamente) y productsManager (para utilizar sus métodos)
 import fs from 'fs';
 import { nanoid } from 'nanoid';
 import productsManager from './productsManager.js';
 
+// Creo una instancia de ProductsManager
 const listProductsToCart = new productsManager();
 
 export default class cartsManager {
@@ -9,51 +11,67 @@ export default class cartsManager {
 		this.path = './src/models/carts.json';
 	}
 
-	// Método privado para leer el archivo de productos
+	// Metodos privados
+	/** Lee el archivo de carritos */
 	#readFileCarts = async () => {
 		const readCart = await fs.promises.readFile(this.path, 'utf-8');
 		return JSON.parse(readCart);
 	};
-	// Método privado para escribir el archivo de productos
+
+	/** Escribe el archivo de carritos
+	 * @param {Array} carts (Arreglo de carritos)
+	 * */
 	#writeFileCarts = async (carts) => {
 		await fs.promises.writeFile(this.path, JSON.stringify(carts));
 	};
-	// Método privado para validar Id
+
+	/** Valida que la id del carrito exista
+	 * @param {number} id (Id del carrito)
+	 */
 	#validIdCart = async (id) => {
 		let carts = await this.#readFileCarts();
 		return carts.find((cart) => cart.id === id);
 	};
+
+	/** Valida que la id del producto exista
+	 * @param {number} id (Id del producto)
+	 */
 	#validIdProduct = async (id) => {
 		let products = await listProductsToCart.getProducts();
-		console.log(products);
 		return await products.find((product) => product.id === id);
 	};
 
+	// Métodos públicos
+	/** Agrega un nuevo carrito */
 	addCart = async () => {
 		const listCarts = await this.#readFileCarts();
 		const cart = {
 			products: [],
 		};
-		cart.id = nanoid();
+		cart.id = nanoid(); // Genera id automáticamente
 		listCarts.push(cart);
 		await this.#writeFileCarts(listCarts);
-		return listCarts;
+		return listCarts; // Retorna el array de carritos incluyendo el nuevo carrito agregado
 	};
 
+	/** Busca carrito por id
+	 * @param {number} id (Id del carrito)
+	 */
 	getCartById = async (id) => {
-		//Almaceno contenido del archivo de productos en una variable
 		let carts = await this.#readFileCarts();
-		//Busco el índice del producto solicitado por id
 		let validCart = await this.#validIdCart(id);
 		if (validCart === false) {
 			return 'Carrito no encontrado';
 		}
-		// Muestro el producto solicitado
-		console.log('Producto Encontrado!');
-		return carts[validCart];
+		return carts[validCart]; // Retorna el carrito indicado
 	};
 
+	/** Agrega un producto a un carrito específico
+	 * @param {number} cid (Id del carrito)
+	 * @param {number} pid (Id del producto)
+	 */
 	addProductToCart = async (cid, pid) => {
+		// Valida id del carrito y del producto
 		let validCart = await this.#validIdCart(cid);
 		if (validCart === false) {
 			return 'Carrito no encontrado';
@@ -64,17 +82,21 @@ export default class cartsManager {
 		}
 
 		const allCarts = await this.#readFileCarts();
+		// Genera un nuevo listado de carritos sin el seleccionado
 		let cartFilter = await allCarts.filter((cart) => cart.id != cid);
 
 		if (validCart.products.some((product) => product.id === pid)) {
+			// Corrobora si el producto está en el carrito
 			let productExist = validCart.products.find(
-				(product) => product.id === pid
+				(product) => product.id === pid // Si está el producto, lo busca y lo trae
 			);
-			productExist.quantity++;
+			productExist.quantity++; // Le suma a la propiedad quantity 1
+			// Integra el nuevo array con el carrito modificado y el resto de carritos
 			let newCarts = [validCart, ...cartFilter];
 			await this.#writeFileCarts(newCarts);
 			return 'Producto Agregado al Carrito';
 		}
+		// Si no está el producto lo agrega e inicializa la propiedad quantity
 		validCart.products.push({ id: validProduct.id, quantity: 1 });
 		let newCarts = [validCart, ...cartFilter];
 		await this.#writeFileCarts(newCarts);
