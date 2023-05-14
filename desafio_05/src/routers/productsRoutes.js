cb853033378a895068b07459c9f5b888cea934df
Mario Aguilar <mariocba150@gmail.com>
2023-05-14 12:43:36 -0300

    Se agregan rutas, archivo css, contenido base de archivos json y plantilla para mostrar productos

--- desafio_05/src/routers/productsRoutes.js
+++ desafio_05/src/routers/productsRoutes.js
@@ -1,5 +1,71 @@
+// Importo Router y productsManager para utilizar sus métodos
 import { Router } from 'express';
+import listOfProducts from '../utils/instances.js';
 
 const productsRoutes = Router();
+const listOfProducts = new productsManager(); // Creo instancia de productsManager
+
+// Ruta para obtener el listado de productos
+productsRoutes.get('/', async (req, res) => {
+	try {
+		const getProductList = await listOfProducts.getProducts();
+		let limit = parseInt(req.query.limit); // Configuro el tipo de valor limit como un número
+		// Si el usuario ingresa un límite de resultados lo muestro así, sino traigo la totalidad de productos
+		if (!limit) {
+			return res.status(200).send({ getProductList });
+		} else {
+			let productsLimit = getProductList.slice(0, limit);
+			res.status(200).send({ productsLimit });
+		}
+	} catch (err) {
+		res.status(400).send({ err });
+	}
+});
+
+// Ruta para obtener un producto mediante su id
+productsRoutes.get('/:pid', async (req, res) => {
+	try {
+		let id = req.params.pid;
+		let obtenerID = await listOfProducts.getProductById(id); // Obtengo el producto
+		// Valido que el producto exista y muestro un error de lo contrario
+		if (!obtenerID) {
+			res.status(400).send({ Resultado: 'ID no encontrada' });
+		}
+		res.status(200).send(await obtenerID);
+	} catch (err) {
+		res.status(400).send({ err });
+	}
+});
+
+// Ruta para agregar un producto
+productsRoutes.post('/', async (req, res) => {
+	try {
+		let newProduct = req.body; // Almaceno el producto que pasan por body
+		res.status(201).send(await listOfProducts.addProduct(newProduct));
+	} catch (err) {
+		res.status(400).send({ err });
+	}
+});
+
+// Ruta para actualizar un producto
+productsRoutes.put('/:pid', async (req, res) => {
+	try {
+		let id = req.params.pid;
+		let newProduct = req.body; // Almaceno el producto actualizado que pasan por body
+		res.status(200).send(await listOfProducts.updateProduct(id, newProduct));
+	} catch (err) {
+		res.status(400).send({ err });
+	}
+});
+
+// Ruta para eliminar un producto
+productsRoutes.delete('/:pid', async (req, res) => {
+	try {
+		let id = req.params.pid;
+		res.status(200).send(await listOfProducts.deleteProduct(id));
+	} catch (err) {
+		res.status(400).send({ err });
+	}
+});
 
 export default productsRoutes;
--- desafio_05/src/routers/viewsRoutes.js
+++ desafio_05/src/routers/viewsRoutes.js
@@ -1,13 +1,15 @@
 import { Router } from 'express';
+import { listOfProducts } from '../utils/instances.js';
 
 const viewsRoutes = Router();
 
 viewsRoutes.get('/', (req, res) => {
-	res.render('home', {});
+	const productsList = listOfProducts.getProducts();
+	res.render('home', { tittle: 'Lista de Productos', products: productsList });
 });
 
 viewsRoutes.get('/realtimeproducts', (req, res) => {
-	res.render('realTimeProducts', {});
+	res.render('realTimeProducts', { tittle: 'Productos en tiempo real' });
 });
 
 export default viewsRoutes;
