c5b26e5afadf7a9a2ac724d7bfae4487426e8511
Mario Aguilar <mariocba150@gmail.com>
2023-05-14 03:01:58 -0300

    agrego vistas y rutas que solicita el desafío

--- desafio_05/src/routers/viewsRoutes.js
+++ desafio_05/src/routers/viewsRoutes.js
@@ -3,7 +3,11 @@ import { Router } from 'express';
 const viewsRoutes = Router();
 
 viewsRoutes.get('/', (req, res) => {
-	res.render('index', {});
+	res.render('home', {});
+});
+
+viewsRoutes.get('/realtimeproducts', (req, res) => {
+	res.render('realTimeProducts', {});
 });
 
 export default viewsRoutes;
